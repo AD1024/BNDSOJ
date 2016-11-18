20d1759769cfd9cc043aafeb361315af4935716f
AD1024 <yoyo991214@126.com>
2016-11-18 13:24:15 +0800

    Some special modifications

--- app/controllers/problem_statement_manage.php
+++ app/controllers/problem_statement_manage.php
@@ -52,13 +52,88 @@
 	<li class="active"><a href="/problem/<?= $problem['id'] ?>/manage/statement" role="tab">编辑</a></li>
 	<li><a href="/problem/<?= $problem['id'] ?>/manage/managers" role="tab">管理者</a></li>
 	<li><a href="/problem/<?= $problem['id'] ?>/manage/data" role="tab">数据</a></li>
-	<? //dhxh begin ?>
-	<li><a href="/problem/<?= $problem['id'] ?>/manage/imgupload" role="tab">图片上传</a></li>
-	<? //dhxh end ?>
-	<?// AD1024 Modify on 2016.10.20 ?>
+	<?php //dhxh begin ?>
+	<!--<li><a href="/problem/<?= $problem['id'] ?>/manage/imgupload" role="tab">图片上传</a></li>-->
+	<?php //dhxh end ?>
+	<?php// AD1024 Modify on 2016.10.20 ?>
 	<li><a href="/problem/<?= $problem['id']?>/manage/imgmanage" role="tab">图片管理</a></li>
-	<?// AD1024 Modify on 2016.10.20 ?>
+	<?php// AD1024 Modify on 2016.10.20 ?>
 	<li><a href="/problem/<?=$problem['id']?>" role="tab">返回</a></li>
 </ul>
+
+<?php //dhxh begin ?>
+<script>window.codee=CodeMirror();</script>
+<?php //dhxh end ?>
+
 <?php $problem_editor->printHTML() ?>
+
+<?php //dhxh begin ?>
+
+<div class="modal fade" id="UploadImgModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    			<h4 class="modal-title" id="myModalLabel">上传数据</h4>
+			</div>
+			<div class="modal-body">
+				<form id="imguploadform" action="" method="post" enctype="multipart/form-data" role="form">
+					<div class="form-group">
+						<label for="exampleInputFile">文件</label>
+						<input type="file" name="problem_img_file" id="problem_img_file">
+						<p class="help-block">请上传.jpg/.jpeg/.gif/.png文件</p>
+					</div>
+					<input type="hidden" name="problem_img_file_submit" value="submit">
+					<button id="uploadimgbtn" type="submit" class="btn btn-success">上传</button>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script language="javascript">
+
+function add_around(sl, sr) {
+	window.codee.replaceSelection(sl + window.codee.getSelection() + sr);
+}
+
+function imgupload(){
+	var imgupload_url="/problem/<?= $problem['id'] ?>/manage/imgupload";
+	$.ajax({
+    	url: imgupload_url,
+    	type: 'POST',
+    	cache: false,
+    	data: new FormData($('#imguploadform')[0]),
+    	processData: false,
+    	contentType: false
+	}).success(function(res) {
+		if(res.substr(1,2)=="ok"){
+			alert("上传成功！");
+			$('#UploadImgModal').modal('hide');
+			window.codee.focus();
+			addcode="![]("+ res.substring(4) +")";
+			add_around("", addcode);
+		}else{
+			alert(res);
+		}
+			
+	}).fail(function(res) {
+		alert("上传失败，请重新上传！");
+	});
+	return false;
+}
+
+$("#uploadimgbtn").click(function() {
+	window.codee.focus();
+	imgupload();
+	return false;
+});
+	
+</script>
+
+<?php //dhxh end ?>
+
 <?php echoUOJPageFooter() ?>
