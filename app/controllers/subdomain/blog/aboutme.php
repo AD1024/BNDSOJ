2bb2da8bd7023fe58236ca5fdf942bea3ca1d5d1
AD1024 <yoyo991214@126.com>
2016-11-18 13:26:28 +0800

    Some special modification on the personal information

--- app/controllers/subdomain/blog/aboutme.php
+++ app/controllers/subdomain/blog/aboutme.php
@@ -1,10 +1,14 @@
 <?php requirePHPLib('form') ?>
 <?php echoUOJPageHeader('关于我') ?>
 
-<?php if (UOJContext::user()['username'] != 'vfleaking'): ?>
-<h3>博主是个超级大神犇！</h3>
+<?php if (UOJContext::user()['username'] == 'teacherone'): ?>
+<h3>博主是个超级大神犇！伟大领袖汪老师!</h3>
 <?php else: ?>
-<h3>博主太弱了！</h3>
+	<?php if (UOJContext::user()['username']!='AD1024'):?>
+		<h3>博主是个超级大神犇!</h3>
+	<?php else: ?>
+		<h3>博主太弱了！</h3>
+	<?php endif ?>
 <?php endif ?>
 
 （好吧目前暂时不支持定制此页，我错了我会加这个功能的 T_T……）
